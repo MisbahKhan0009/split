bc3abff0b782e36863c173330eee41843d44debe
MisbahKhan0009 <9.6840923e+07+MisbahKhan0009@users.noreply.github.com>
2026-08-17 08:02:33 +0000

    Fix local live service startup

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,12 +1,18 @@
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from apps.core.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+
+django_asgi_application = get_asgi_application()
+
+from apps.core.routing import websocket_urlpatterns
+
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_application,
     "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
